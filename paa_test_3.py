48b6a8e2e96b66c26fb2378b20ec22de423bca35
kimyk0120 <kimyk0120@hotmail.com>
2019-02-20 13:22:15 +0900

    python audio analysis

--- paa_test_3.py
+++ paa_test_3.py
@@ -9,7 +9,9 @@ Extra trees
 Gradient boosting
 '''
 
-# Train Segment Classifier From Data
+'''
+    Train Segment Classifier From Data
+'''
 # list_of_dirs:        list of paths of directories.
 # Each directory contains a signle audio class whose samples are stored in seperate WAV files.
 # mt_win, mt_step:        mid-term window length and step
@@ -18,7 +20,7 @@ Gradient boosting
 # model_name:        name of the model to be saved
 
 '''
-# Example:
+# Example: MusicGenre, Classes
 from pyAudioAnalysis import audioTrainTest as aT
 aT.featureAndTrain(["/home/tyiannak/Desktop/MusicGenre/Classical/","/home/tyiannak/Desktop/MusicGenre/Electronic/","/home/tyiannak/Desktop/MusicGenre/Jazz/"], 1.0, 1.0, aT.shortTermWindow, aT.shortTermStep, "svm", "svmMusicGenre3", True)
 aT.featureAndTrain(["/home/tyiannak/Desktop/MusicGenre/Classical/","/home/tyiannak/Desktop/MusicGenre/Electronic/","/home/tyiannak/Desktop/MusicGenre/Jazz/"], 1.0, 1.0, aT.shortTermWindow, aT.shortTermStep, "knn", "knnMusicGenre3", True)
@@ -31,24 +33,14 @@ aT.featureAndTrain(["/home/tyiannak/Desktop/5Class/Silence/","/home/tyiannak/Des
 aT.featureAndTrain(["/home/tyiannak/Desktop/5Class/Silence/","/home/tyiannak/Desktop/5Class/SpeechMale/","/home/tyiannak/Desktop/5Class/SpeechFemale/","/home/tyiannak/Desktop/5Class/ObjectsOther/","/home/tyiannak/Desktop/5Class/Music/"], 1.0, 1.0, aT.shortTermWindow, aT.shortTermStep, "gradientboosting", "gb5Classes")
 aT.featureAndTrain(["/home/tyiannak/Desktop/5Class/Silence/","/home/tyiannak/Desktop/5Class/SpeechMale/","/home/tyiannak/Desktop/5Class/SpeechFemale/","/home/tyiannak/Desktop/5Class/ObjectsOther/","/home/tyiannak/Desktop/5Class/Music/"], 1.0, 1.0, aT.shortTermWindow, aT.shortTermStep, "randomforest", "rf5Classes")
 
-# Command-line use:
+# Command-line use: SM, 
 # python audioAnalysis.py trainClassifier -i <directory1> ... <directoryN> --method <svm, svm_rbf, knn, extratrees, gradientboosting or randomforest> -o <modelName> --beat (optional for beat extraction)
 # python audioAnalysis.py trainClassifier -i classifierData/speech/ classifierData/music/ --method svm -o data/svmSM
 # python audioAnalysis.py trainClassifier -i classifierData/speech/ classifierData/music/ --method knn -o data/knnSM
+
 # python audioAnalysis.py trainClassifier -i /media/tyiannak/My\ Passport/ResearchData/AUDIO/musicGenreClassificationData/ALL_DATA/WAVs/Electronic/ /media/tyiannak/My\ Passport/ResearchData/AUDIO/musicGenreClassificationData/ALL_DATA/WAVs/Classical/ /media/tyiannak/My\ Passport/ResearchData/AUDIO/musicGenreClassificationData/ALL_DATA/WAVs/Jazz/ --method svm --beat -o data/svmMusicGenre3
 # python audioAnalysis.py trainClassifier -i /media/tyiannak/My\ Passport/ResearchData/AUDIO/musicGenreClassificationData/ALL_DATA/WAVs/Electronic/ /media/tyiannak/My\ Passport/ResearchData/AUDIO/musicGenreClassificationData/ALL_DATA/WAVs/Classical/ /media/tyiannak/My\ Passport/ResearchData/AUDIO/musicGenreClassificationData/ALL_DATA/WAVs/Jazz/ --method knn --beat -o data/knnMusicGenre3
 '''
-
-
-
-
-
-
-
-
-
-
-
 
 
 
--- paa_test_4.py
+++ paa_test_4.py
@@ -5,14 +5,14 @@
     Single File Classification
 
 '''
-
+import subprocess
 from pyAudioAnalysis import audioTrainTest as aT
 
 Result, P, classNames = aT.fileClassification("../../audio-source/wave/a2002011001-e02.wav", "pyAudioAnalysis/data/svmMusicGenre6","svm")
 # Result, P, classNames = aT.fileClassification("../../audio-source/wave/noexcuses.wav", "pyAudioAnalysis/data/svmMusicGenre6","svm")
-print(Result) # 1.0  class ID
-print(P) # [0.08675024 0.55253466 0.1331328  0.04243957 0.16545349 0.01968925] probability estimate
-print(classNames) # ['Blues', 'Classical', 'Electronic', 'Jazz', 'Rap', 'Rock']
+# print(Result) # 1.0  class ID
+# print(P) # [0.08675024 0.55253466 0.1331328  0.04243957 0.16545349 0.01968925] probability estimate
+# print(classNames) # ['Blues', 'Classical', 'Electronic', 'Jazz', 'Rap', 'Rock']
 
 # Command-line use:
 # python audioAnalysis.py classifyFile -i <inputFilePath> --model <svm, svm_rbf, knn, extratrees, gradientboosting or randomforest> --classifier <pathToClassifierModeL>
@@ -24,6 +24,17 @@ print(classNames) # ['Blues', 'Classical', 'Electronic', 'Jazz', 'Rap', 'Rock']
 '''
     Folder Classification
 '''
+
+# Command-line use examples:
+# python audioAnalysis.py classifyFolder -i testFolder/ --model svm --classifier data/svmSM            (only generates freq counts for each audio class)
+# python audioAnalysis.py classifyFolder -i testFolder/ --model svm --classifier data/svmSM --details  (also outputs the result of each singe WAV file)
+
+# Command-Line
+# ex) data/speechEmotion/
+# speech              		47
+# music               		0
+subprocess.call("cd pyAudioAnalysis; "
+                "python audioAnalysis.py classifyFolder -i ../../../audio-source/SMTest/ --model svm --classifier data/svmSM --details", shell=True)
 
 
 
