fb4d789599391a3d0a31651ec1168699035696df
kimyk0120 <kimyk0120@hotmail.com>
2019-02-21 15:46:21 +0900

    python audio analysis

--- paa_test_7.py
+++ paa_test_7.py
@@ -12,12 +12,13 @@ from pyAudioAnalysis import audioSegmentation as aS
 '''
 
 # [flagsInd, classesAll, acc, CM] = aS.mtFileClassification("../../audio-source/SMTest/voice_speech.mp3", "pyAudioAnalysis/data/svmSM", "svm", True, 'output/voice_speech.segments')
-# [flagsInd, classesAll, acc, CM] = aS.mtFileClassification("pyAudioAnalysis/data/scottish.wav", "pyAudioAnalysis/data/svmSM", "svm", True, 'output/scottish.segments')
+[flagsInd, classesAll, acc, CM] = aS.mtFileClassification("pyAudioAnalysis/data/scottish.wav", "pyAudioAnalysis/data/svmSM", "svm", True, 'output/scottish.segments')
 
 # Command-line use:
 # python audioAnalysis.py segmentClassifyFile -i <inputFile> --model <model type (svm or knn)> --modelName <path to classifier model>
 # Example:
 # python audioAnalysis.py segmentClassifyFile -i data/scottish.wav --model svm --modelName data/svmSM
 
-subprocess.call("cd pyAudioAnalysis; "
-                "python audioAnalysis.py segmentClassifyFile -i data/scottish.wav --model svm --modelName data/svmSM", shell=True)
+# subprocess.call("cd pyAudioAnalysis; "
+#                 "python audioAnalysis.py segmentClassifyFile -i data/scottish.wav --model svm --modelName data/svmSM", shell=True)
+
